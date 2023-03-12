03c1954e398b7023adb38b7d5f56399fd1ba12a1
Christian <112215625+christianrocha383@users.noreply.github.com>
2023-03-12 18:27:53 -0300

    Add files via upload

---
+++ Javascript/app.js
@@ -0,0 +1,12 @@
+/*
+
+Obtendo um novo array, com a mesma quantidade de itens
+do array original usando map.
+
+*/
+
+const numbers = [5, 7, 3]
+
+const quadrado = numbers.map (number => number ** 2)
+
+console.log (quadrado)
